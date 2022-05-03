0c825c9469630e83652d64f106f9141bbad44ca1
Gustavo Lodi Vidaletti <gustavo.vidaletti@gmail.com>
2022-05-03 01:55:43 -0300

    fix(Calculo): Divisão de todo o tanque estava acontecendo

--- src/main/java/CentroDistribuicao.java
+++ src/main/java/CentroDistribuicao.java
@@ -331,10 +331,10 @@ public class CentroDistribuicao {
         return valores;
       }
 
-      aditivo = (int) ((aditivo - targetAditivo) / 2);
-      alcool1 = (int) ((alcool1 - a1) / 2);
-      alcool2 = (int) ((alcool2 - a2) / 2);
-      gasolina = (int) ((gasolina - targetGasolina) / 2);
+      aditivo = (int) (aditivo - targetAditivo / 2);
+      alcool1 = (int) (alcool1 - (a1 / 2));
+      alcool2 = (int) (alcool2 - (a2 / 2));
+      gasolina = (int) (gasolina - (targetGasolina / 2));
 
       defineSituacao();
 
