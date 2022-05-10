8953fabca0e23c2d6e8f977ef080570295d33c88
Gustavo Lodi Vidaletti <gustavo.vidaletti@gmail.com>
2022-05-10 18:48:40 -0300

    correção de dependências

--- src/test/java/CentroDistribuicaoTests.java
+++ src/test/java/CentroDistribuicaoTests.java
@@ -1,7 +1,5 @@
-import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
